6ac1aadc54feb807cf9a0b07b6bba0c309b2ba85
Nadav Fima <nadavfima@gmail.com>
2025-07-26 23:10:10 +0200

    Add new asteroid assets and update AsteroidGenerator to include them. Updated asteroid scene files with new textures and adjusted properties for improved gameplay. Enhanced asteroid generation logic to support additional asteroid types (7-11).

--- src/asteroid/AsteroidGenerator.gd
+++ src/asteroid/AsteroidGenerator.gd
@@ -8,12 +8,17 @@ class AsteroidGenerator:
 	var Asteroid4 = load("res://src/asteroid/Asteroid4.tscn")
 	var Asteroid5 = load("res://src/asteroid/Asteroid5.tscn")
 	var Asteroid6 = load("res://src/asteroid/Asteroid6.tscn")
+	var Asteroid7 = load("res://src/asteroid/Asteroid7.tscn")
+	var Asteroid8 = load("res://src/asteroid/Asteroid8.tscn")
+	var Asteroid9 = load("res://src/asteroid/Asteroid9.tscn")
+	var Asteroid10 = load("res://src/asteroid/Asteroid10.tscn")
+	var Asteroid11 = load("res://src/asteroid/Asteroid11.tscn")
 	
 
 	func generateAsteroid():
 		
 		randomize()
-		var randomAsteroid = randi() % 3 + 1
+		var randomAsteroid = randi() % 11 + 1
 		
 		#print("random asteroid ", randomAsteroid)
 		
@@ -31,6 +36,16 @@ class AsteroidGenerator:
 				newAsteroid = Asteroid5.instantiate()
 			6:
 				newAsteroid = Asteroid6.instantiate()
+			7:
+				newAsteroid = Asteroid7.instantiate()
+			8:
+				newAsteroid = Asteroid8.instantiate()
+			9:
+				newAsteroid = Asteroid9.instantiate()
+			10:
+				newAsteroid = Asteroid10.instantiate()
+			11:
+				newAsteroid = Asteroid11.instantiate()
 
 		pass
 		
