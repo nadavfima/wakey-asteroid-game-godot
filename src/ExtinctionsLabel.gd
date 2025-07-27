1a1752e201639b4bf3f2bf4b08aee26b597725fe
Nadav Fima <nadavfima@gmail.com>
2025-07-27 15:52:18 +0200

    Enhance ExtinctionsLabel and UserScoreLabel for improved visual presentation. Update font settings and layout properties in both labels for better appearance. Adjust offsets in MainScene.tscn for consistent UI alignment.

--- src/ExtinctionsLabel.gd
+++ src/ExtinctionsLabel.gd
@@ -2,14 +2,30 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Set up the RichTextLabel properties for better appearance
+	bbcode_enabled = true
+	scroll_active = false
+	fit_content = true
+	
+	# Set custom font
+	var font = load("res://assets/Fredoka-Bold.ttf")
+	if font:
+		add_theme_font_override("normal_font", font)
+		add_theme_font_size_override("normal_font_size", 28)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var main_scene = get_parent().get_parent()
+	var remaining = 3
 	if main_scene.has_method("get_mass_extinctions"):
-		var remaining = 3 - main_scene.massExtinctions
-		text = str("[center]","Extinctions Remaining\n", remaining,"[/center]")
-	else:
-		text = str("[center]","Extinctions Remaining\n", 3,"[/center]")
+		remaining = 3 - main_scene.massExtinctions
+	
+	# Create a more visually appealing extinctions display
+	text = str(
+		"[center]",
+		"[font_size=18][color=#FEC15D]EXTINCTIONS[/color][/font_size]\n",
+		"[font_size=18][color=#FEC15D]REMAINING[/color][/font_size]\n",
+		"[font_size=48][color=#FFFFF3]", remaining, "[/color][/font_size]",
+		"[/center]"
+	)
 	pass
--- src/UserScoreLabel.gd
+++ src/UserScoreLabel.gd
@@ -2,13 +2,28 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Set up the RichTextLabel properties for better appearance
+	bbcode_enabled = true
+	scroll_active = false
+	fit_content = true
+	
+	# Set custom font
+	var font = load("res://assets/Fredoka-Bold.ttf")
+	if font:
+		add_theme_font_override("normal_font", font)
+		add_theme_font_size_override("normal_font_size", 28)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var main_scene = get_parent().get_parent()
+	var score = 0
 	if main_scene.has_method("get_user_score"):
-		text = str("[center]","User Score\n", main_scene.userScore,"[/center]")
-	else:
-		text = str("[center]","User Score\n", 0,"[/center]")
-	pass
+		score = main_scene.userScore
+	
+	# Create a more visually appealing score display
+	text = str(
+		"[center]",
+		"[font_size=20][color=#FEC15D]USER SCORE[/color][/font_size]\n",
+		"[font_size=48][color=#FFFFF3]", score, "[/color][/font_size]",
+		"[/center]"
+	)
